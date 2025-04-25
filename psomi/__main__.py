336443d23205ec4f67816965cc20dcb146a70935
zeropointnothing <ZeroPointNothing@outlook.com>
2025-04-24 22:38:57 -0400

    make token file config instead

--- psomi/__main__.py
+++ psomi/__main__.py
@@ -6,14 +6,13 @@ from psomi.utils.data import Data
 
 intents = discord.Intents.default() #Defining intents
 intents.message_content = True # Adding the message_content intent so that the bot can read user messages
-test = commands.Bot(command_prefix="p!", intents=intents)
+bot = commands.Bot(command_prefix="p!", intents=intents)
 
 if __name__ == "__main__":
     print(command_groups)
     for group in command_groups:
-        test.load_extension(group)
+        bot.load_extension(group)
 
-    dt = Data("database.db")
 
-    with open("token.json", "r") as f:
-        test.run(json.load(f)["token"])
+    with open("config.json", "r") as f:
+        bot.run(json.load(f)["token"])
