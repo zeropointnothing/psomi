a1a7c7038201f7d7d33965b92dba9e7b393f16e2
zeropointnothing <ZeroPointNothing@outlook.com>
2025-04-28 01:12:07 -0400

    add ProxyGroup deletion command

--- psomi/commands/grouping.py
+++ psomi/commands/grouping.py
@@ -1,3 +1,5 @@
+from asyncio import timeout
+
 import discord
 from discord.ext import commands
 from psomi.utils.bot import PsomiBot
@@ -23,6 +25,44 @@ class Grouping(commands.Cog):
             return
         
         await ctx.reply(f"Successfully created the '{name}' ProxyGroup!")
+
+    @commands.command(name="delete", description="Delete an existing ProxyGroup.")
+    async def delete_command(self, ctx: commands.Context, title: str):
+        try:
+            user = self.bot.database.get_user(str(ctx.message.author.id))
+        except NotFoundError:
+            await ctx.reply("You do not currently have any created ProxyGroups! Try creating one first!")
+            return
+
+        try:
+            group = self.bot.database.get_proxygroup(user, title)
+        except NotFoundError:
+            await ctx.reply(f"You do not have a ProxyGroup with the title '{title}'!")
+            return
+
+        await ctx.reply(f"Are you sure you'd like to delete this ProxyGroup? ('{title}')\n"
+                        "This action is irreversible! All Characters within this group will become uncategorized!\n"
+                        "Reply with 'I am sure!' to proceed, or 'cancel' to abort (or wait 20 seconds).")
+
+        try:
+            # destructive action, we should wait for confirmation first.
+            result: discord.Message = await self.bot.wait_for(
+                "message",
+                check=lambda x: x.channel.id == ctx.channel.id
+                and x.author.id == ctx.author.id
+                and x.content.lower() in ["i am sure!", "cancel"],
+                timeout=20.0
+            )
+        except TimeoutError:
+            await ctx.send("Aborting due to lack of accepted response...")
+            return
+
+        if result.content.lower() == "i am sure!":
+            self.bot.database.delete_proxygroup(user, group)
+
+            await result.reply(f"Successfully deleted the '{title}' ProxyGroup!")
+        else:
+            await result.reply("Canceled!")
 
 def setup(bot):
     bot.add_cog(Grouping(bot))
