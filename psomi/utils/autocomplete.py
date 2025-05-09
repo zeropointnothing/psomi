1e7e50d6208694392348859362026db84a17ad44
zeropointnothing <ZeroPointNothing@outlook.com>
2025-05-09 15:20:57 -0400

    remove debug print statements

--- psomi/utils/autocomplete.py
+++ psomi/utils/autocomplete.py
@@ -6,10 +6,10 @@ def chr_name_autocomplete(ctx: discord.AutocompleteContext):
     bot = cast(PsomiBot, ctx.bot)
 
     if str(ctx.interaction.user.id) in bot.user_cache:
-        print("cache hit! (character name)")
+        # print("cache hit! (character name)")
         user = bot.user_cache[str(ctx.interaction.user.id)]
     else:
-        print("cache miss! (character name)")
+        # print("cache miss! (character name)")
         user = bot.database.get_user(str(ctx.interaction.user.id))
         bot.user_cache[user.uid] = user
     return [_[0].name for _ in user.get_character_by_search(ctx.value)]
@@ -18,10 +18,10 @@ def pgp_name_autocomplete(ctx: discord.AutocompleteContext):
     bot = cast(PsomiBot, ctx.bot)
 
     if str(ctx.interaction.user.id) in bot.user_cache:
-        print("cache hit! (proxygroup name)")
+        # print("cache hit! (proxygroup name)")
         user = bot.user_cache[str(ctx.interaction.user.id)]
     else:
-        print("cache miss! (proxygroup name)")
+        # print("cache miss! (proxygroup name)")
         user = bot.database.get_user(str(ctx.interaction.user.id))
         bot.user_cache[user.uid] = user
     return [_[0].title for _ in user.get_proxygroup_by_search(ctx.value)]
